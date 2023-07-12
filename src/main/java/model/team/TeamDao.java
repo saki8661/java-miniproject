57c69422ad1a18e47c5e67326af1d9a778fb2c75
saki8661 <saki8661@naver.com>
2023-07-12 11:17:18 +0900

    teamDAO추가 register로 메소드 용어 통일 studium 코드 수정 1~4번 까지 전체 run 해보고 정상 실행확인 완료

--- src/main/java/model/team/TeamDao.java
+++ src/main/java/model/team/TeamDao.java
@@ -1,8 +1,9 @@
 package model.team;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamDao {
     private  Connection connection;
@@ -11,5 +12,41 @@ public class TeamDao {
     }
 
     // 팀 등록
-    // 전체 팀 목록
+    public void registerTeam(int stadiumId, String name, Timestamp createdAt) throws SQLException {
+        String query = "INSERT INTO team(stadium_id, name, created_at) VALUES(?, ?, ?)";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, stadiumId);
+            statement.setString(2, name);
+            statement.setTimestamp(3, createdAt);
+            statement.executeUpdate();
+        }
+    }
+
+    // 전체 팀 목록보기
+    public List<Team> getAllTeams() throws SQLException {
+        List<Team> teams = new ArrayList<>();
+        String query = "SELECT * FROM team";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    Team team = buildTeamFromResultSet(resultSet);
+                    teams.add(team);
+                }
+            }
+        }
+        return teams;
+    }
+
+
+    private Team buildTeamFromResultSet(ResultSet resultSet) throws SQLException {
+        int stadium_id = resultSet.getInt("stadium_id");
+        String name = resultSet.getString("name");
+        Timestamp createdAt = resultSet.getTimestamp("created_at");
+
+        Team team = new Team(stadium_id, name, createdAt);
+
+        return team;
+    }
+
 }
